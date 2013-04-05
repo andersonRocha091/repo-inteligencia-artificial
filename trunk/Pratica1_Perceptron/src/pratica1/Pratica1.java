dba36a9e4e361eb177b693a9f4d78d06f31bab7c
andersonecomp091@gmail.com <andersonecomp091@gmail.com@d40e72a3-404f-9b86-5b98-cee68cb243c2>
2013-04-05 02:11:47 +0000

    Revisado treino, implementada a classificação por treino... tem que revisar esta parte.

--- trunk/Pratica1_Perceptron/src/pratica1/Pratica1.java
+++ trunk/Pratica1_Perceptron/src/pratica1/Pratica1.java
@@ -14,6 +14,7 @@ public class Pratica1 {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("treinamento.txt")));
         ArrayList<String[]> entrada = new ArrayList<String[]>();
+        ArrayList<String[]> entradaClassificacao = new ArrayList<String[]>();
         String[] line = null;
         String temp;
 
@@ -21,6 +22,13 @@ public class Pratica1 {
             line = temp.split("\\ ");
             entrada.add(line);
         }
+        
+        reader = new BufferedReader(new InputStreamReader(new FileInputStream("teste.txt"))); //leitura das entradas a classificar
+            
+            while ((temp = reader.readLine()) != null) {
+                line = temp.split("\\ ");
+                entradaClassificacao.add(line);
+            }
 
         int qtdPeso = entrada.get(0).length - 1;
         float[] vetorPeso = new float[qtdPeso];
@@ -37,12 +45,13 @@ public class Pratica1 {
 
         //treinamento
         for (int k = 0; k < qtdTreino; k++) {
-            System.out.println("Inicial");
+            System.out.println("\nInicial");
             imprimePesos(vetorPeso);
             do {
+                erro = false;
                 for (int i = 0; i < entrada.size(); i++) {
                     for (int j = 0; j < entrada.get(i).length - 1; j++) {
-                        u += (Float.parseFloat(entrada.get(i)[j]))*vetorPeso[j];
+                        u += (Float.parseFloat(entrada.get(i)[j])) * vetorPeso[j];
                         y = u > 0 ? 1 : -1;
                         float valorDesejado = Float.parseFloat(entrada.get(i)[entrada.get(0).length - 1]);
                         if (y != valorDesejado) {
@@ -57,8 +66,29 @@ public class Pratica1 {
             System.out.println("Final");
             imprimePesos(vetorPeso);
             System.out.println("");
+            //Fase de Classificação
+            System.out.println("Classificação das Amostras Treino:"+(k+1));
+            
+            
+            for (int i = 0; i < entradaClassificacao.size(); i++) { //Classificação das amostras;
+                    for (int j = 0; j < entradaClassificacao.get(i).length - 1; j++) {
+                        u += (Float.parseFloat(entradaClassificacao.get(i)[j])) * vetorPeso[j];
+                        //float valorDesejado = Float.parseFloat(entradaClassificacao.get(i)[entradaClassificacao.get(0).length - 1]);
+                        //if (y != valorDesejado) {
+                          //  float valorEntrada = Float.parseFloat(entrada.get(i)[j]);
+                            //vetorPeso[j] = vetorPeso[j] + aprendizagem * (valorDesejado - y) * valorEntrada;
+                            //erro = true;
+                       // }
+                    }
+                    y = u > 0 ? 1 : -1;
+                    System.out.println("Amostra:"+(i+1)+" "+"Classificação:"+y); //exibição para cada treino
+                    
+                }
+            
         }
         reader.close();
+
+
     }
 
     public static void imprimePesos(float[] vetorPeso) {

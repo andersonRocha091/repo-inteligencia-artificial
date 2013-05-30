e428b2d87fc4e163cd34e4f7cd4ed6e97bd91c6d
maocleal <maocleal@d40e72a3-404f-9b86-5b98-cee68cb243c2>
2013-05-30 14:52:56 +0000

    Arquivo de treinamento menor

--- Kohonen/src/kohonen/Kohonen.java
+++ Kohonen/src/kohonen/Kohonen.java
@@ -6,45 +6,44 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
 /**
  *
  * @author Anderson e Maria
  */
-public class Kohonen{
+public class Kohonen {
 
     // método que converte uma ArrayList<float[]> para String formato impressao
     public static String arraylistToString(ArrayList<float[]> resultado) {
-         String resultadoString = "";
-         for (float[] y : resultado) {
-             resultadoString += ""+Arrays.toString(y)+"\n";
-         }
-         return "["+resultadoString.replaceAll("]",";").replaceAll("]]","],\n").replaceAll("\\["," ")+"]\n";
+        String resultadoString = "";
+        for (float[] y : resultado) {
+            resultadoString += "" + Arrays.toString(y) + "\n";
+        }
+        return "[" + resultadoString.replaceAll("]", ";").replaceAll("]]", "],\n").replaceAll("\\[", " ") + "]\n";
     }
-    
+
     // método que converte uma ArrayList<float[][]> para String formato impressao
     public static String arraylistMatrizToString(ArrayList<float[][]> resultado) {
-         String resultadoString = "[";
-         for (float[][] y : resultado) {
-             resultadoString += ""+Arrays.deepToString(y).replaceAll("],",";\n").replaceAll("]]","],\n").replaceAll("\\["," ")+"\n";
-         }
-         return resultadoString+"]";
+        String resultadoString = "[";
+        for (float[][] y : resultado) {
+            resultadoString += "" + Arrays.deepToString(y).replaceAll("],", ";\n").replaceAll("]]", "],\n").replaceAll("\\[", " ") + "\n";
+        }
+        return resultadoString + "]";
     }
-    
-    public static String matrizToString(float[][] matriz){
-        return "["+Arrays.deepToString(matriz).replaceAll("],",";\n").replaceAll("]]","],\n").replaceAll("\\["," ")+"}\n";
-    }
-    
-    public static int[] classificacaoAmostras(Neuronio[][] mapaTreinado, ArrayList<float[]> entradaTeste) {                
-        int[] neuroniosVencedores = new int[entradaTeste.size()];
-        
-        return neuroniosVencedores;
-    }   
 
-    public static ArrayList<float[]> leArquivo(String path) throws Exception {        
-        ArrayList<float[]> dadosArquivo = new ArrayList<float[]>();        
+    public static String matrizToString(float[][] matriz) {
+        return "[" + Arrays.deepToString(matriz).replaceAll("],", ";\n").replaceAll("]]", "],\n").replaceAll("\\[", " ") + "}\n";
+    }
+
+    public static int[] classificacaoAmostras(Neuronio[][] mapaTreinado, ArrayList<float[]> entradaTeste) {
+        int[] neuroniosVencedores = new int[entradaTeste.size()];
+
+        return neuroniosVencedores;
+    }
+
+    public static ArrayList<float[]> leArquivo(String path) throws Exception {
+        ArrayList<float[]> dadosArquivo = new ArrayList<float[]>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(
-                new FileInputStream(path)));        
+                new FileInputStream(path)));
         String[] line = null;
         String temp;
         while ((temp = reader.readLine()) != null) {
@@ -57,20 +56,20 @@ public class Kohonen{
         }
         return dadosArquivo;
     }
-    
-    public static Neuronio[][] inicializarRede(int qtdNeuronios, int qtdPesosNeuronios){
-        int qtdLinhas = (int)Math.sqrt(qtdNeuronios);
+
+    public static Neuronio[][] inicializarRede(int qtdNeuronios, int qtdPesosNeuronios) {
+        int qtdLinhas = (int) Math.sqrt(qtdNeuronios);
         Neuronio[][] matrizNeuronios = new Neuronio[qtdLinhas][qtdLinhas];
-        int indiceNeuronio=1;
+        int indiceNeuronio = 1;
         for (int i = 0; i < qtdLinhas; i++) {
             for (int j = 0; j < qtdLinhas; j++) {
-                
+
                 Neuronio n = new Neuronio(qtdPesosNeuronios, indiceNeuronio++);
                 matrizNeuronios[i][j] = n;
-                
-                
+
+
             }
-        }             
+        }
         return matrizNeuronios;
     }
 
@@ -84,89 +83,79 @@ public class Kohonen{
         return resultado;
     }
 
-    
     /*public static float[] novoPeso(float[] peso, float taxaAprendizagem, float[] entrada) {        
-        for (int i = 0; i < peso.length; i++) {
-            peso[i] = peso[i] + taxaAprendizagem *( entrada[i] - peso[i]);                            
-        }        
-        return peso;
+    for (int i = 0; i < peso.length; i++) {
+    peso[i] = peso[i] + taxaAprendizagem *( entrada[i] - peso[i]);
+    }
+    return peso;
     }*/
-    
-    public static Neuronio[][] kohonen(ArrayList<float[]> entrada, Neuronio[][] rede, float aprendizagem){
+    public static Neuronio[][] kohonen(ArrayList<float[]> entrada, Neuronio[][] rede, float aprendizagem) {
         int[] vencedoresAnteriores = new int[entrada.size()];
         int[] vencedoresAtuais = new int[entrada.size()];
-        float normaAnterior =  Float.POSITIVE_INFINITY;
         float norma = 0;
-        int[] deslocamentoX = {0,0,1,0,-1};
-        int[] deslocamentoY = {0,1,0,-1,0};
-        float menorNorma=0;
-        int indiceVencedor=0;
-        int indiceVencedorX=0;
-        int indiceVencedorY=0;
-        int epocas=0;
-                
-        do{
-            vencedoresAnteriores = Arrays.copyOf(vencedoresAtuais,vencedoresAtuais.length);
-            
+        int[] deslocamentoX = {0, 0, 1, 0, -1};
+        int[] deslocamentoY = {0, 1, 0, -1, 0};
+        float menorNorma = 0;
+        int indiceVencedor = 0;
+        int indiceVencedorX = 0;
+        int indiceVencedorY = 0;
+        int epocas = 0;
+
+        do {
+            vencedoresAnteriores = Arrays.copyOf(vencedoresAtuais, vencedoresAtuais.length);
             for (int i = 0; i < entrada.size(); i++) {
-                menorNorma=Float.POSITIVE_INFINITY;
-                for(int j=0;j<rede.length;j++){
-                    for(int k=0;k<rede[0].length;k++){
-                       
-                       norma = calcularDistancia(entrada.get(i), rede[j][k].getVetorPesos());
-                       
-                       if(norma < menorNorma){
-                           menorNorma = norma;
-                           indiceVencedor = rede[j][k].getIndice(); 
-                           indiceVencedorX=k;
-                           indiceVencedorY=j;
-                       }
+                menorNorma = Float.POSITIVE_INFINITY;
+                for (int j = 0; j < rede.length; j++) {
+                    for (int k = 0; k < rede[0].length; k++) {
+                        norma = calcularDistancia(entrada.get(i), rede[j][k].getVetorPesos());
+                        if (norma < menorNorma) {
+                            menorNorma = norma;
+                            indiceVencedor = rede[j][k].getIndice();
+                            indiceVencedorX = k;
+                            indiceVencedorY = j;
+                        }
                     }
                 }
                 vencedoresAtuais[i] = indiceVencedor;
-               
-              //  System.out.println("vencedor x: "+indiceVencedorX+"vencedor y: "+indiceVencedorY);
-            //    System.out.println("norma:"+norma);
-                
-                for(int t=0; t<deslocamentoX.length;t++){
-                    try{
-                        rede[indiceVencedorY+deslocamentoY[t]][indiceVencedorX+deslocamentoX[t]].atualizarPesos(entrada.get(i), menorNorma);
-                    }catch(ArrayIndexOutOfBoundsException e){
+                System.out.println("indice vencedor: "+indiceVencedor);
+                //  System.out.println("vencedor x: "+indiceVencedorX+"vencedor y: "+indiceVencedorY);
+                //  System.out.println("norma:"+norma);
+
+                for (int t = 0; t < deslocamentoX.length; t++) {
+                    try {
+                        rede[indiceVencedorY + deslocamentoY[t]][indiceVencedorX + deslocamentoX[t]].atualizarPesos(entrada.get(i), menorNorma);
+                    } catch (ArrayIndexOutOfBoundsException e) {                        
                     }
-                
                 }
-                
-                
-                }
-            System.out.println("vetorAtual"+Arrays.toString(vencedoresAtuais));
-                
-                norma=0;
-               // normaAnterior = Float.POSITIVE_INFINITY;
-                menorNorma=0;
-                epocas++;
-                
-            
-            
-        }while(!Arrays.equals(vencedoresAnteriores, vencedoresAtuais));
-    
+            }
+            norma = 0;
+            menorNorma = 0;
+            indiceVencedor=-1;
+            indiceVencedorX=-1;
+            indiceVencedorY=-1;
+            epocas++;
+            System.out.println("Vencedores a: "+Arrays.toString(vencedoresAtuais));
+            System.out.println("Vencedores b: "+Arrays.toString(vencedoresAnteriores));
+            System.out.println("Qnt epocas atual: "+epocas);
+        } while (!Arrays.equals(vencedoresAnteriores, vencedoresAtuais));
+        System.out.println("------- O treinamento teve "+epocas+" epocas.----------");
         return rede;
     }
 
-    public static float calcularDistancia(float[] entrada, float[] peso){
-      
+    public static float calcularDistancia(float[] entrada, float[] peso) {
+
         float distancia = 0;
-        float acumulador=0;
-        
-        for(int i=0; i<entrada.length;i++){
-          acumulador += Math.pow(entrada[i]-peso[i], 2);
+        float acumulador = 0;
+
+        for (int i = 0; i < entrada.length; i++) {
+            acumulador += Math.pow(entrada[i] - peso[i], 2);
         }
-        distancia = (float)Math.sqrt(acumulador);
-        
-    return distancia;
+        distancia = (float) Math.sqrt(acumulador);
+
+        return distancia;
     }
-    
-    
-    public static void main(String[] args) throws Exception {        
+
+    public static void main(String[] args) throws Exception {
         // Definição dos parâmetros do algoritmo
         float taxaAprendizagem = (float) 0.001;
         int qtdNeuronios = 16; // grid topologico 4x4
@@ -174,18 +163,19 @@ public class Kohonen{
 
         // Leitura de arquivos de entrada        
         ArrayList<float[]> entrada = leArquivo("treina.txt");
-        ArrayList<float[]> entradaTeste = leArquivo("teste.txt");       
+        //ArrayList<float[]> entrada = leArquivo("treinamento.txt");
+        ArrayList<float[]> entradaTeste = leArquivo("teste.txt");
 
         Neuronio[][] mapaTopologico = inicializarRede(qtdNeuronios, qtdPesosNeuronio);
-        
-        System.out.println("Mapa inicializado:\n "+Arrays.deepToString(mapaTopologico).replaceAll("],",";\n").replaceAll("]]","],\n").replaceAll("\\["," ")+"\n");
-        
+
+        System.out.println("Mapa inicializado:\n " + Arrays.deepToString(mapaTopologico).replaceAll("],", ";\n").replaceAll("]]", "],\n").replaceAll("\\[", " ") + "\n");
+
         Neuronio[][] mapaTreinado = kohonen(entrada, mapaTopologico, taxaAprendizagem);
-        
-       System.out.println("Mapa treinado:\n "+Arrays.deepToString(mapaTreinado).replaceAll("],",";\n").replaceAll("]]","],\n").replaceAll("\\["," ")+"\n");
-        
-       int[] neuroniosVencedores = classificacaoAmostras(mapaTreinado,entradaTeste);
-        
-       System.out.println("Neuronios vencedores classificacao \n"+Arrays.toString(neuroniosVencedores));
-    }    
+
+        System.out.println("Mapa treinado:\n " + Arrays.deepToString(mapaTreinado).replaceAll("],", ";\n").replaceAll("]]", "],\n").replaceAll("\\[", " ") + "\n");
+
+        int[] neuroniosVencedores = classificacaoAmostras(mapaTreinado, entradaTeste);
+
+        System.out.println("Neuronios vencedores classificacao \n" + Arrays.toString(neuroniosVencedores));
+    }
 }

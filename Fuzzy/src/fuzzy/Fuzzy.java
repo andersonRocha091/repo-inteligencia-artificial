32b656a0c0da5e8e3384a6e45e1d10dd4864b000
maocleal <maocleal@d40e72a3-404f-9b86-5b98-cee68cb243c2>
2013-07-07 07:27:05 +0000

    Algumas arrumações na organização do código e arrumações nas impressões para a entrega.

--- Fuzzy/src/fuzzy/Fuzzy.java
+++ Fuzzy/src/fuzzy/Fuzzy.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package fuzzy;
 
 import java.util.ArrayList;
@@ -9,13 +5,10 @@ import java.util.Arrays;
 
 /**
  *
- * @author Anderson
+ * @author Anderson e Maria Alice
  */
 public class Fuzzy {
 
-    /**
-     * @param args the command line arguments
-     */
     private static double[] discursoTemperatura;
     private static double[] discursoVolume;
     private static double[] discursoPressao;
@@ -27,12 +20,37 @@ public class Fuzzy {
     private static double[] grandeVolume;
     private static double[] baixaPressao;
     private static double[] mediaPressao;
-    private static double[] altaPressao;
+    private static double[] altaPressao;    
     private static int[][] regrasInferencia;
 
     public static void main(String[] args) {
-        // baixa = 0, media = 1, alta = 2
-        regrasInferencia = new int[][]{{0,3,0}, // 1ª regra
+        
+        // conjuntos fuzzy
+        discursoTemperatura = new double[500];
+        baixaTemperatura = new double[500];
+        mediaTemperatura = new double[500];
+        altaTemperatura = new double[500];
+        pequenoVolume = new double[500];
+        medioVolume = new double[500];
+        grandeVolume = new double[500];
+        baixaPressao = new double[500];
+        mediaPressao = new double[500];
+        altaPressao = new double[500];
+
+        // entrada de temperatura e volume
+        double[][] entrada = new double[][]{{965.00,11.00},
+                                             {920.00,7.6},
+                                             {1050.00,6.3},
+                                             {843.00,8.6},
+                                             {1122.00,5.2}};
+
+        // listas de vetores resultantes em que o alphacorte nao foi igual a 0
+        ArrayList<double[]> conjuntosSaidaAlphaCorte = new ArrayList<double[]>();
+
+        // Regras de inferencia armazenadas no formato de matriz
+        // baixa = 0, media = 1, alta = 2 para temperatura e pressao
+        // pequeno = 3, medio = 4, grande = 5 para volume
+        regrasInferencia = new int[][]{{0,3,0}, // 1ª regra - temperatura, volume, pressao
                                        {1,3,0}, // 2ª regra
                                        {2,3,1}, // 3ª regra
                                        {0,4,0}, // 4ª regra
@@ -43,59 +61,43 @@ public class Fuzzy {
                                        {2,5,2}, // 9ª regra
                                         };
 
+        // etapas de fuzificação da temperatura, volume e pressao
         fuzificacaoTemperatura();
         fuzificacaoVolume();
         fuzificacaoPressao();
 
-        double[][] entrada; // entrada de temperatura e volume
-        entrada = new double[][]{{965.00,11.00},
-                                 {920.00,7.6},
-                                 {1050.00,6.3},
-                                 {843.00,8.6},
-                                 {1122.00,5.2}};
-
-        double[][] resultado = calculaPertinenciaEntrada(entrada);
-        
+        // armazena pertinencia da entrada referente a todos os conjuntos fuzzy de temperatura,
+        // voume e pressao na matriz resultados
+        double[][] resultado = calculaPertinenciaEntrada(entrada);        
         imprimeMatrizResultado(resultado);
 
-        ArrayList<double[]> conjuntosSaidaAlphaCorte = new ArrayList<double[]>();
-
+        // aplicação das regras de inferencia mandani, agregação max e desfuzificação
         for(int i=0; i<5; i++){
             conjuntosSaidaAlphaCorte.clear();
             for(int z=0; z<9; z++){
                 // aplicacao da regra de inferencia z
                 double alphacorte = Math.min(resultado[i][regrasInferencia[z][0]], resultado[i][regrasInferencia[z][1]]);
-                //System.out.println("alpha corte da regra "+z+": "+alphacorte);
-
-                //cria o conjunto
+                //cria o conjunto resultado após aplicação do alphacorte
                 if(alphacorte>0){
                     double[] novoConjunto = criaConjuntoAlphaCorte(alphacorte, retornaConjuntoPressao(regrasInferencia[z][2]));
-                    //System.out.println("\nImprimindo novo conjunto na regra: "+z+" com alphacorte = "+alphacorte+":\n");
-                    //for (int i = 0; i < novoConjunto.length; i++) {
-                        //System.out.println("Discurso: "+discursoPressao[i]+", Novoconjunto: "+novoConjunto[i]);
-                        //System.out.println(novoConjunto[i]+",");
-
-                    //}
-                    // adiciona na lista de conjuntos saida alpha corte
                     conjuntosSaidaAlphaCorte.add(novoConjunto);
                 }
             }
-            //fazer agregação dos conjuntos das regras ativadas
+            // agregação nos conjuntos das regras ativadas
             double[] resultadoAgregacao = agregacaoMax(conjuntosSaidaAlphaCorte);
-            //System.out.println("Resultado agregacao: \n"+Arrays.toString(resultadoAgregacao).replace(",", ",\n"));
+            System.out.println("Resultado da agregacao para a entrada "+i+": \n"+Arrays.toString(resultadoAgregacao).replace(",", ",\n"));
+
+            // desfuzificação
             double resultadoFinal = desfuzificacao(resultadoAgregacao);
-            System.out.println("Resultado finalzao da entrada "+i+" :"+resultadoFinal);
+            System.out.println("\nPressao desfuzificada referente a entrada "+i+" : "+resultadoFinal+" atm.\n");
         }
-
-        //System.out.println("resultado: "+procura(mediaTemperatura, discursoTemperatura, 1151.00));
-
     }
 
+    // Desfuzificacao centro de area
     public static double desfuzificacao(double[] resultadoAgregacao){
         double resultadoFinal;
         double acumulaNumerador = 0;
         double acumulaDenominador = 0;
-
         for (int i = 0; i < resultadoAgregacao.length; i++) {
             acumulaNumerador += discursoPressao[i]*resultadoAgregacao[i];
             acumulaDenominador += resultadoAgregacao[i];
@@ -107,7 +109,6 @@ public class Fuzzy {
     public static double[] agregacaoMax(ArrayList<double[]> conjuntos){
         double[] vetorFinal = new double[conjuntos.get(0).length];
         double max = 0;
-
         for (int i = 0; i < vetorFinal.length; i++) {
             if(conjuntos.size()>1){
                 max = conjuntos.get(0)[i];
@@ -121,9 +122,7 @@ public class Fuzzy {
             else{
                 vetorFinal[i]=conjuntos.get(0)[i];
             }
-
         }
-
         return vetorFinal;
     }
 
@@ -139,7 +138,6 @@ public class Fuzzy {
 
     public static double[] criaConjuntoAlphaCorte(double alphaCorte, double[] conjuntoVelho){
         double[] conjuntoNovo = new double[conjuntoVelho.length];
-
         for (int i = 0; i < conjuntoNovo.length; i++) {
             if(alphaCorte<conjuntoVelho[i]){
                 conjuntoNovo[i]=alphaCorte;
@@ -148,27 +146,24 @@ public class Fuzzy {
                 conjuntoNovo[i]=conjuntoVelho[i];
             }
         }
-
         return conjuntoNovo;
     }
 
     public static void imprimeMatrizResultado(double[][] resultado){
          for (int i = 0; i < resultado.length; i++) {
-            System.out.println("Imprimindo resultados para entrada da linha "+i);
+            System.out.println("\nGraus de pertinencia para entrada "+i);
             System.out.println("baixa temperatura: "+resultado[i][0]);
             System.out.println("media temperatura: "+resultado[i][1]);
             System.out.println("alta temperatura: "+resultado[i][2]);
             System.out.println("pequeno volume: "+resultado[i][3]);
             System.out.println("medio volume: "+resultado[i][4]);
-            System.out.println("grande volume: "+resultado[i][5]);
-            
+            System.out.println("grande volume: "+resultado[i][5]);            
         }
-
+         System.out.println("");
     }
 
     public static double[][] calculaPertinenciaEntrada(double[][] entrada){
         double[][] resultado = new double[entrada.length][6];
-
         for (int i = 0; i < entrada.length; i++) {
             // calculando o grau de pertinencia do conj baixa temperatura para a entrada i de temperatura
             resultado[i][0] = procura(baixaTemperatura, discursoTemperatura, entrada[i][0]);
@@ -183,18 +178,17 @@ public class Fuzzy {
             // calculando o grau de pertinencia do conj grande volume para a entrada i de volume
             resultado[i][5] = procura(grandeVolume, discursoVolume, entrada[i][1]);
         }
-
         return resultado;
     }
 
-    // retorna o grau de pertinencia ao conjunto pesquisado
+    // retorna o grau de pertinencia do conjunto referente a uma entrada
     public static double procura(double[] conjunto, double[] discurso, double entrada){
         int indiceAchou = -1;
         for (int i = 1; i < discurso.length; i++) {
             if(discurso[i]>=entrada){                
                 double diferenca1 = discurso[i]-entrada;    
                 double diferenca2 = discurso[i-1]-entrada;    
-                if(Math.abs(diferenca1)>Math.abs(diferenca2)){
+                if(Math.abs(diferenca1)>Math.abs(diferenca2)){ // pega o mais proximo
                     indiceAchou = i-1;
                 }
                 else{
@@ -206,58 +200,53 @@ public class Fuzzy {
         return conjunto[indiceAchou];
     }
 
-    public static void fuzificacaoTemperatura() {
-
-        discursoTemperatura = new double[500];
-        baixaTemperatura = new double[500];
-        mediaTemperatura = new double[500];
-        altaTemperatura = new double[500];
-        pequenoVolume = new double[500];
-        medioVolume = new double[500];
-        grandeVolume = new double[500];
-        baixaPressao = new double[500];
-        mediaPressao = new double[500];
-        altaPressao = new double[500];
-        
+    public static void fuzificacaoTemperatura() {        
         double fatorIncremento = (1200.00 - 800.00) / 499.00;
 
         discursoTemperatura[0] = 800;
-      //  System.out.println("fator incremento:" + fatorIncremento);
+        // System.out.println("fator incremento:" + fatorIncremento);
         for (int i = 1; i < 499; i++) {
             discursoTemperatura[i] = discursoTemperatura[i - 1] + fatorIncremento;
         }
         discursoTemperatura[499]=1200.00;
-        //gerando o conjunto fuzzy para baixa temperatura
-       // System.out.println("\n Baixa temperatura: \n");
+        System.out.println("\nConjunto de Baixa Temperatura: \n");
         baixaTemperatura=trapezoidal(discursoTemperatura, 800.00, 1000.00, 800.00,900.00);
-        //System.out.println("baixaTemperatura:"+ Arrays.toString(baixaTemperatura).replaceAll(",", ",\n"));
-       mediaTemperatura = triangular(discursoTemperatura, 900.00, 1100.00, 1000.00);
-      //  System.out.println("\n alta temperatura: \n");
-       altaTemperatura = trapezoidal(discursoTemperatura, 1000.00, 1200.00, 1100.00, 1200.00);
-        
-
-        // System.out.println("vetorTemperatura:"+ Arrays.toString(discursoTemperatura).replaceAll(",", ",\n"));
+        System.out.println("\nConjunto de Media Temperatura: \n");
+        mediaTemperatura = triangular(discursoTemperatura, 900.00, 1100.00, 1000.00);
+        System.out.println("\nConjunto de Alta Temperatura: \n");
+        altaTemperatura = trapezoidal(discursoTemperatura, 1000.00, 1200.00, 1100.00, 1200.00);
     }
 
     public static void fuzificacaoVolume() {
-
         discursoVolume = new double[500];
         double fatorIncremento = (12.00 - 2.00) / 499.00;
-
         discursoVolume[0] = 2;
-      //  System.out.println("fator incremento:" + fatorIncremento);
         for (int i = 1; i < 499; i++) {
-
             discursoVolume[i] = discursoVolume[i - 1] + fatorIncremento;
-
         }
         discursoVolume[499]=12.00;
-        System.out.println("\npequeno volume:\n" );
+        System.out.println("\nConjunto de Pequeno Volume:\n" );
         pequenoVolume=trapezoidal(discursoVolume, 2.00, 7.00, 2.00, 4.50);
-        System.out.println("\n medio volume:\n" );
-       medioVolume = triangular(discursoVolume, 4.50, 9.50, 7.00);
-       System.out.println("\n grande Volume: \n");
-       grandeVolume = trapezoidal(discursoVolume, 7.0, 12.00, 9.50, 12.00);
+        System.out.println("\nConjunto de Medio Volume:\n" );
+        medioVolume = triangular(discursoVolume, 4.50, 9.50, 7.00);
+        System.out.println("\nConjunto de Grande Volume: \n");
+        grandeVolume = trapezoidal(discursoVolume, 7.0, 12.00, 9.50, 12.00);
+    }
+
+    public static void fuzificacaoPressao() {
+        discursoPressao = new double[500];
+        double fatorIncremento = (12.00 - 4.00) / 499.00;
+        discursoPressao[0] = 4;
+        for (int i = 1; i < 499; i++) {
+            discursoPressao[i] = discursoPressao[i - 1] + fatorIncremento;
+        }
+        discursoPressao[499]=12.00;
+        System.out.println("\nConjunto de Baixa Pressao:\n" );
+        baixaPressao=trapezoidal(discursoPressao, 4.00, 8.00, 4.00, 5.00);
+        System.out.println("\nConjunto de Media Pressao:\n" );
+        mediaPressao = triangular(discursoPressao, 6.00, 10.00, 8.00);
+        System.out.println("\nConjunto de Alta Pressao: \n");
+        altaPressao = trapezoidal(discursoPressao, 8.00, 12.00, 11.00, 12.00);
     }
     
     public static double[] trapezoidal(double[] discurso, double a, double b, double m, double n){
@@ -280,7 +269,8 @@ public class Fuzzy {
                     resultado[i]=0;
                 }
                 //System.out.println(discurso[i]+", "+resultado[i]);
-                System.out.println(resultado[i]+",");
+                //System.out.println(resultado[i]+",");
+                System.out.println("Discurso: "+discurso[i]+", Pertinencia: "+resultado[i]);
             }
         return resultado;
     }
@@ -299,33 +289,9 @@ public class Fuzzy {
                 else if(discurso[i]>b){
                     resultado[i]=0;
                 }
-             //System.out.println(discurso[i]+", "+resultado[i]);
-              System.out.println(resultado[i]+",");
+             System.out.println("Discurso: "+discurso[i]+", Pertinencia: "+resultado[i]);
+             // System.out.println(resultado[i]+",");
             }
-        return resultado;
-        
-    }
-
-    public static void fuzificacaoPressao() {
-
-        discursoPressao = new double[500];
-        double fatorIncremento = (12.00 - 4.00) / 499.00;
-
-        discursoPressao[0] = 4;
-      //  System.out.println("fator incremento:" + fatorIncremento);
-        for (int i = 1; i < 499; i++) {
-
-            discursoPressao[i] = discursoPressao[i - 1] + fatorIncremento;
-
-        }
-        discursoPressao[499]=12.00;
-        
-        System.out.println("\nbaixa pressao:\n" );
-        baixaPressao=trapezoidal(discursoPressao, 4.00, 8.00, 4.00, 5.00);
-        System.out.println("\n media pressao:\n" );
-       mediaPressao = triangular(discursoPressao, 6.00, 10.00, 8.00);
-       System.out.println("\n alta Pressao: \n");
-       altaPressao = trapezoidal(discursoPressao, 8.00, 12.00, 11.00, 12.00);
-        // System.out.println("vetorPressao:"+ Arrays.toString(discursoPressao).replaceAll(",", ",\n"));
+        return resultado;   
     }
 }

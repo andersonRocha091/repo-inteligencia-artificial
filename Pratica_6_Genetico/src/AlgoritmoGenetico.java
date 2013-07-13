1971ff7e2b0d5bff308904e09dc5469815105f09
maocleal <maocleal@d40e72a3-404f-9b86-5b98-cee68cb243c2>
2013-07-13 15:28:55 +0000

    Gerando a população, com alguns bugs na conversão numérica de volta pra float.

--- Pratica_6_Genetico/src/AlgoritmoGenetico.java
+++ Pratica_6_Genetico/src/AlgoritmoGenetico.java
@@ -1,3 +1,6 @@
+
+import java.text.DecimalFormat;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,7 +16,39 @@ public class AlgoritmoGenetico {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int geracoesMax = 200;
+        int tamanhoPopulacao = 100;
+        int x1 = -1;
+        int x2 = 2;
+
+        String[] populacao = gerarPopulacao(tamanhoPopulacao,(float)x1,(float)x2);
+
+        System.out.println(binarioParaFloat("10111101111111000101010110000110"));
+
+    }
+
+    public static String[] gerarPopulacao(int tamanhoPopulacao, float x1, float x2){
+        String[] populacao = new String[tamanhoPopulacao];
+        for (int i = 0; i < tamanhoPopulacao; i++) {
+            float novoIndividuo = (x2+1)*((float)Math.random())+x1;
+            DecimalFormat df = new DecimalFormat("0.000000");
+            String formatado = df.format((double)novoIndividuo);
+            populacao[i] = gerarBinario(Float.parseFloat(formatado.replace(",", ".")));
+            //System.out.println("individuo "+i+":"+populacao[i]);//+"; float: "+binarioParaFloat(populacao[i]));
+        }        
+        return populacao;
+    }
+
+    public static float sorteia(float x1, float x2){
+        return x2*((float)Math.random())+x1;
+    }
+
+    public static String gerarBinario(float num){
+        return Integer.toBinaryString(Float.floatToIntBits(num));
+    }
+
+    public static float binarioParaFloat(String binario){
+        return Float.intBitsToFloat(Integer.parseInt(binario,2));
     }
 
 }

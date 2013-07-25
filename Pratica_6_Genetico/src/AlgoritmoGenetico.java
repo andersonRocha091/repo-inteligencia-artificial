20eaf561ca5d4cdf9dba20c4014452fad1a6ab93
maocleal <maocleal@d40e72a3-404f-9b86-5b98-cee68cb243c2>
2013-07-25 02:13:52 +0000

    Código que eu adiantei na aula de terça, ontem..

--- Pratica_6_Genetico/src/AlgoritmoGenetico.java
+++ Pratica_6_Genetico/src/AlgoritmoGenetico.java
@@ -15,13 +15,18 @@ import java.util.Collections;
 public class AlgoritmoGenetico {
     
     
-
+    private static ArrayList<Individuo> populacao;
+    private static ArrayList<Individuo> populacaoSelecionada;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         int geracoesMax = 200;
         int tamanhoPopulacao = 100;
+        int populacaoIntermediaria = 20; // quantidade individuos q sera selecionada c a roleta
+    
+        
         int geracoes = 0; 
         int x1 = -1;
         int x2 = 2;
@@ -37,55 +42,120 @@ public class AlgoritmoGenetico {
         }*/
         
         // gerou a populacao
-        ArrayList<Individuo> populacao = gerarPopulacao(tamanhoPopulacao);
+        gerarPopulacao(tamanhoPopulacao);
         imprimirPopulacao(populacao);        
         System.out.println("");
         
         // avaliou e ordenou
-        funcaoAvaliacao(populacao);        
+        funcaoAvaliacao();        
         imprimirPopulacao(populacao);        
         System.out.println("");
         Collections.sort(populacao);        
         imprimirPopulacao(populacao);
         
-        while(geracoes++<200){
+        // loop principal com criterio de parada
+        while(geracoes<geracoesMax){     
+            geracoes++;
             
+            //seleciona a populacao
+            selecao(populacaoIntermediaria);
             
-        }
-        
+            //aplica cruzamento
+            cruzamentoPopulacao();
+            
+            //aplica mutacao
+            mutacao();
+            
+            //avalia populacao e ordena
+            funcaoAvaliacao();        
+            imprimirPopulacao(populacao);        
+            System.out.println("");
+            Collections.sort(populacao);        
+            imprimirPopulacao(populacao);
+            
+            //descarta os menos aptos
+            descartarMenosAptos();            
+        }        
         //System.out.println("Populacao convertida:\n"+Arrays.toString(converterPopulacaoBinarioFloat(populacao)));
+    }
+    
+    public static void mutacao(){
+        
         
     }
     
-    public float quantidadePosicoesParaCadaIndividuo(float tamanhoVetor, float probabilidadeIndividuo, float somatorio){
+    public static void cruzamentoPopulacao(){
+        //for (int i = 0; i < ; i++) {
+            
+        //}
+    }
+    
+    public static Individuo cruzamentoIndividuo(Individuo a, Individuo b){
+        Individuo novoIndividuo = new Individuo();
+        int tamanhoCromossomo = a.getCromossomo().length;
+        int[] cromossomoNovoIndividuo = new int[tamanhoCromossomo];
+        int pontoIntermediarioCruzamento = tamanhoCromossomo/2;
+        for (int i = 0; i < pontoIntermediarioCruzamento; i++) {
+            cromossomoNovoIndividuo[i] = b.getCromossomo()[i];
+        }        
+        for (int i = pontoIntermediarioCruzamento; i < tamanhoCromossomo; i++) {
+            cromossomoNovoIndividuo[i] = a.getCromossomo()[i];            
+        }
+        novoIndividuo.setCromossomo(cromossomoNovoIndividuo);
+        return novoIndividuo;
+    } 
+    
+    public static void descartarMenosAptos(){
+        for (int i = 100; i < populacao.size(); i++) {
+            populacao.remove(i);
+        }
+    }
+    
+    public static float quantidadePosicoesParaCadaIndividuo(float tamanhoVetor, float probabilidadeIndividuo, float somatorio){
         return (probabilidadeIndividuo*tamanhoVetor)/somatorio;
     }
     
-    public ArrayList<Individuo> roleta(ArrayList<Individuo> populacaoAtual){
+    // adiciona individuos selecionados pela roleta no novo conjunto de individuos selecionados
+    public static void selecao(int populacaoIntermediaria){
+        int[] vetorSorteio = preparaRoleta();
+        populacaoSelecionada = new ArrayList<Individuo>();
+        for (int i = 0; i < populacaoIntermediaria; i++) {
+            populacaoSelecionada.add(aplicaRoleta(vetorSorteio));            
+        }
+    }
+    
+    // prepara vetor para sorteio da roleta, a partir do somatorio das aptidoes e da 
+    // probabilidade de cada individuo
+    public static int[] preparaRoleta(){
         int tamanhoVetorRoleta = 1000;
         int[] vetorSorteio = new int[tamanhoVetorRoleta];        
         float somatorioAptidao = 0;
         int ultimaPosicaoOcupada = -1;
        
-        for (int i = 0; i < populacaoAtual.size(); i++) {
-            somatorioAptidao+=populacaoAtual.get(i).getAptidao();            
+        for (int i = 0; i < populacao.size(); i++) {
+            somatorioAptidao+=populacao.get(i).getAptidao();            
         }
         
-        for (int i = 0; i < populacaoAtual.size(); i++) {
-            float probabilidadeIndividuo = populacaoAtual.get(i).getAptidao()/somatorioAptidao;
+        for (int i = 0; i < populacao.size(); i++) {
+            float probabilidadeIndividuo = populacao.get(i).getAptidao()/somatorioAptidao;
             float qtdIndividuo = quantidadePosicoesParaCadaIndividuo(tamanhoVetorRoleta, probabilidadeIndividuo, somatorioAptidao);
             while(qtdIndividuo-->=0){
                 vetorSorteio[ultimaPosicaoOcupada+1] = i;
                 ultimaPosicaoOcupada+=1;
+                System.out.println("posicao preenchida: "+ultimaPosicaoOcupada);
             }
         }
+        return vetorSorteio;
+    }
+    
+    public static Individuo aplicaRoleta(int[] vetorSorteio){        
         // sorteio da posicao do vetor
         int indiceSorteado = (int)Math.random()*1000;
-        if(vetorSorteio[indiceSorteado]==0){
-            
-        }
         
-        return (ArrayList<Individuo>) populacaoAtual.clone();
+        //if(vetorSorteio[indiceSorteado]==0){            
+        //}
+        
+        return populacaoSelecionada.get(indiceSorteado);
     }
     
     public static void imprimirPopulacao(ArrayList<Individuo> populacao){
@@ -95,21 +165,21 @@ public class AlgoritmoGenetico {
           System.out.println(populacao.toString().replace(".,",",\n"));
     }
     
-    // ou funcao avaliacao
-    public static void funcaoAvaliacao(ArrayList<Individuo> populacao){
+    // funcao avaliacao
+    public static void funcaoAvaliacao(){
         converterPopulacaoBinarioFloat(populacao);
         for (int i = 0; i < populacao.size(); i++) {
             populacao.get(i).setAptidao(funcaoAptidao((populacao.get(i).getCromossomoFloat())));
         }
     }
     
-    // f(x) = x*sen(10*Math.pi*x)+1
+    // calculando valor da funcao aptida f(x) = x*sen(10*Math.pi*x)+1
     public static float funcaoAptidao(float x){
         return x*(float)Math.sin(10*Math.PI*x)+1;
     }
 
     public static ArrayList<Individuo> gerarPopulacao(int tamanhoPopulacao){
-        ArrayList<Individuo> populacao = new ArrayList<Individuo>();
+        populacao = new ArrayList<Individuo>();
         for (int i = 0; i < tamanhoPopulacao; i++) {
             int[] cromossomoNovoIndividuo = new int[22];
             for (int j = 0; j < 22; j++) {
